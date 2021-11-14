eb5ad754af956bbb450967be07042f35f32cfd1e
poetahto <walls.dk@gmail.com>
2021-11-14 12:48:11 -0800

    basic level loading system ported

---
+++ Assets/Scripts/LevelLoadTrigger.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoadTrigger : Trigger
+{
+    [SerializeField] public Level currentLevel;
+
+    private LevelController _levelController;
+    
+    private void Start()
+    {
+        _levelController = LevelController.Get();
+        // layersThatCanTrigger = 1 << LayerMask.NameToLayer("Player");
+        
+        events.collisionEnter.AddListener(OnLevelEnter);
+        events.collisionExit.AddListener(OnLevelExit);
+    }
+
+    private void OnDisable()
+    {
+        events.collisionEnter.RemoveListener(OnLevelEnter);
+        events.collisionExit.RemoveListener(OnLevelExit);
+    }
+
+    private void OnLevelExit(GameObject obj)
+    {
+        _levelController.UnloadLevel(currentLevel);
+    }
+
+    private void OnLevelEnter(GameObject obj)
+    {
+        _levelController.LoadLevel(currentLevel);
+    }
+
+    public void GenerateCollider()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        GameObject[] gameObjects = scene.GetRootGameObjects();
+
+        Bounds bounds = new Bounds(gameObjects[0].transform.position, Vector3.zero);
+        
+        foreach (var rootObj in gameObjects)
+        {
+            foreach (var childObj in rootObj.GetComponentsInChildren<Renderer>())
+                bounds.Encapsulate(childObj.bounds);    
+        }
+
+        colliderComponent = GenerateEdgeCollider2D(bounds);
+    }
+
+    private PolygonCollider2D GenerateEdgeCollider2D(Bounds bounds)
+    {
+        if (!TryGetComponent(out PolygonCollider2D edgeCollider2D))
+            edgeCollider2D = gameObject.AddComponent<PolygonCollider2D>();
+
+        var colliderSizeX = bounds.max.x - bounds.min.x;
+        var colliderSizeY = bounds.max.y - bounds.min.y;
+
+        var colliderPoints = new Vector2[4];
+        colliderPoints[0] = new Vector2(-colliderSizeX / 2, colliderSizeY / 2) + (Vector2) bounds.center;
+        colliderPoints[1] = new Vector2(colliderSizeX / 2, colliderSizeY / 2) + (Vector2) bounds.center;
+        colliderPoints[2] = new Vector2(colliderSizeX / 2, -colliderSizeY / 2) + (Vector2) bounds.center;
+        colliderPoints[3] = new Vector2(-colliderSizeX / 2, -colliderSizeY / 2) + (Vector2) bounds.center;
+        
+        edgeCollider2D.points = colliderPoints;
+        
+        return edgeCollider2D;
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public struct CollisionEvents 
+{
+    public UnityEvent<GameObject> collisionEnter;
+    public UnityEvent<GameObject> collisionExit;
+}
+
+[RequireComponent(typeof(Collider2D))]
+// Wraps a Collider and exposes UnityEvents for OnTriggerEnter / Exit 
+public class Trigger : MonoBehaviour
+{
+    [Header("Trigger Events")]
+    [SerializeField] public CollisionEvents events;
+    
+    [Header("Trigger Settings")]
+    [SerializeField] public Collider2D colliderComponent;
+    [SerializeField] public LayerMask layersThatCanTrigger = ~0;
+    
+    private readonly List<GameObject> _objectsInTrigger = new List<GameObject>(); 
+    private GameObject _collidedObject;
+
+    public bool HasObjectInTrigger(GameObject obj)
+    {
+        return _objectsInTrigger.Contains(obj);
+    }
+    
+    protected virtual void Awake()
+    {
+        if (colliderComponent == null)
+            colliderComponent = GetComponent<Collider2D>();
+
+        colliderComponent.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _collidedObject = other.gameObject;
+
+        if (ObjectCanTrigger())
+            AddObjectAndFireEvent();    
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _collidedObject = other.gameObject;
+
+        if (ObjectCanTrigger())
+            RemoveObjectAndFireEvent();
+    }
+
+    private bool ObjectCanTrigger()
+    {
+        // Make sure the collided object falls within our layer mask.
+        return (1 << _collidedObject.layer & layersThatCanTrigger) != 0;
+    }
+
+    private void AddObjectAndFireEvent()
+    {
+        _objectsInTrigger.Add(_collidedObject);
+        events.collisionEnter?.Invoke(_collidedObject);
+    }
+    
+    private void RemoveObjectAndFireEvent()
+    {
+        _objectsInTrigger.Remove(_collidedObject);
+        events.collisionExit?.Invoke(_collidedObject);
+    }
+}
