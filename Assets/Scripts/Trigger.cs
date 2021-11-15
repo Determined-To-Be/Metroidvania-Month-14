d1a4699ad8a5cd7472b2990f5d4c83c0cb151ee4
poetahto <walls.dk@gmail.com>
2021-11-15 00:19:56 -0800

    cleanup up trigger code

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,73 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Events;
 
-[Serializable]
-public struct CollisionEvents 
-{
-    public UnityEvent<GameObject> collisionEnter;
-    public UnityEvent<GameObject> collisionExit;
-}
+/// <summary>
+/// Wraps a Collider and exposes UnityEvents for OnTriggerEnter / Exit
+/// </summary>
 
 [RequireComponent(typeof(Collider2D))]
-// Wraps a Collider and exposes UnityEvents for OnTriggerEnter / Exit 
 public class Trigger : MonoBehaviour
 {
-    [Header("Trigger Events")]
-    [SerializeField] public CollisionEvents events;
+    [Header("Settings")]
+    [Tooltip("Only GameObjects on this layer will cause this Trigger to update its state.")]
+    public LayerMask layersThatCanTrigger = ~0;
     
-    [Header("Trigger Settings")]
-    [SerializeField] public Collider2D colliderComponent;
-    [SerializeField] public LayerMask layersThatCanTrigger = ~0;
+    [Foldout("Collision Events")]
+    [Tooltip("This event is invoked when an object enters this collider.")]
+    public UnityEvent<GameObject> collisionEnter;
+    
+    [Foldout("Collision Events")]
+    [Tooltip("This event is invoked when an object exits this collider.")]
+    public UnityEvent<GameObject> collisionExit;
     
     private readonly List<GameObject> _objectsInTrigger = new List<GameObject>(); 
-    private GameObject _collidedObject;
-
+    
+    /// <summary>
+    /// Checks if a GameObject is currently inside this Trigger.
+    /// </summary>
+    /// <param name="obj">The GameObject to check for.</param>
+    /// <returns>True if the requested GameObject is inside the Trigger, false otherwise.</returns>
+    
+    [PublicAPI]
     public bool HasObjectInTrigger(GameObject obj)
     {
         return _objectsInTrigger.Contains(obj);
     }
     
-    protected virtual void Awake()
+    private void Awake()
     {
-        if (colliderComponent == null)
-            colliderComponent = GetComponent<Collider2D>();
-
-        colliderComponent.isTrigger = true;
+        GetComponent<Collider2D>().isTrigger = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        _collidedObject = other.gameObject;
-
-        if (ObjectCanTrigger())
-            AddObjectAndFireEvent();    
+        GameObject obj = other.gameObject;
+        
+        if (ObjectCanTrigger(obj))
+            AddObjectAndFireEvent(obj);    
     }
     
     private void OnTriggerExit2D(Collider2D other)
     {
-        _collidedObject = other.gameObject;
-
-        if (ObjectCanTrigger())
-            RemoveObjectAndFireEvent();
+        GameObject obj = other.gameObject;
+        
+        if (ObjectCanTrigger(obj))
+            RemoveObjectAndFireEvent(obj);
     }
 
-    private bool ObjectCanTrigger()
+    private bool ObjectCanTrigger(GameObject obj)
     {
         // Make sure the collided object falls within our layer mask.
-        return (1 << _collidedObject.layer & layersThatCanTrigger) != 0;
+        return (1 << obj.layer & layersThatCanTrigger) != 0;
     }
 
-    private void AddObjectAndFireEvent()
+    private void AddObjectAndFireEvent(GameObject obj)
     {
-        _objectsInTrigger.Add(_collidedObject);
-        events.collisionEnter?.Invoke(_collidedObject);
+        _objectsInTrigger.Add(obj);
+        collisionEnter?.Invoke(obj);
     }
     
-    private void RemoveObjectAndFireEvent()
+    private void RemoveObjectAndFireEvent(GameObject obj)
     {
-        _objectsInTrigger.Remove(_collidedObject);
-        events.collisionExit?.Invoke(_collidedObject);
+        _objectsInTrigger.Remove(obj);
+        collisionExit?.Invoke(obj);
     }
 }
